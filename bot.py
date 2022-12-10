d4cd8c33db829fa5fe9489a4031b26ba80145566
joshstubble <62226937+joshstubble@users.noreply.github.com>
2022-12-10 08:38:50 +0000

    idk

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def sports_alert(ctx):
     # Indented code goes here
 
     # Process any messages that are sent to the bot
-    @client.event
+ @client.event
     async def on_message(message):
     # Ignore messages from the bot itself
     if message.author == client.user:
