13479bfed9c8210b1162706da6f4659d4001282a
joshstubble <62226937+joshstubble@users.noreply.github.com>
2022-12-13 04:30:55 +0000

    change css

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ async def startscores(ctx):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Find the home and away scores for the first game on the scoreboard
-        home_team_score_element = soup.select_one('.home .score')
+        home_team_score_element = soup.select_one('ScoreCell__Score h4 clr-gray-01 fw-heavy tar ScoreCell_Score--scoreboard pl2')
         away_team_score_element = soup.select_one('.away .score')
         home_score = home_team_score_element.text
         away_score = away_team_score_element.text
